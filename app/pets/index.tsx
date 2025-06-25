51c5a6d37d6064bc9f7942fca1850f72a3d2cc9c
rjfuhrman42 <reidjf77@gmail.com>
2025-06-25 19:43:03 -0400

    pagination attempt 1

--- app/pets/index.tsx
+++ app/pets/index.tsx
@@ -1,24 +1,77 @@
 import { Link } from "expo-router";
-import { FlatList, StatusBar, StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  Button,
+  FlatList,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { pets } from "../../data/pets";
+
+const NUM_PETS_PER_PAGE = 100;
 
 export default function PetsList() {
+  const [pets, setPets] = useState();
+  const [page, setPage] = useState(0);
+
+  const [totalPets, setTotalPets] = useState();
+
+  useEffect(() => {
+    async function fetchPets() {
+      const data = await fetch(
+        `https://dummyjson.com/todos?limit=${NUM_PETS_PER_PAGE}&skip=${
+          page * NUM_PETS_PER_PAGE
+        }`
+      );
+      const newPets = await data.json();
+
+      if (!newPets) return;
+      if (!totalPets) setTotalPets(newPets.total);
+
+      console.log("todos", newPets);
+      setPets((prev) => {
+        if (!prev) {
+          return newPets.todos;
+        }
+        return [...prev, ...newPets.todos];
+      });
+    }
+    fetchPets();
+  }, [page]);
+
   return (
     <SafeAreaProvider style={styles.container}>
-      <FlatList
-        contentContainerStyle={styles.petList}
-        data={pets}
-        renderItem={({ item }) => (
-          <Link href={`/pets/${item.id}`} key={item.id}>
-            <View style={styles.card}>
-              <Text style={{ fontWeight: "bold" }}>{item.name}</Text>
-              <Text>ID: {item.id}</Text>
-            </View>
-          </Link>
-        )}
-        keyExtractor={(item) => item.id.toString()}
-      />
+      {!pets ? (
+        <Text>Loading pets</Text>
+      ) : (
+        <View style={styles.inner}>
+          <FlatList
+            contentContainerStyle={styles.petList}
+            data={pets}
+            ListFooterComponent={
+              pets.length >= totalPets ? (
+                <></>
+              ) : (
+                <Button
+                  title="Load more pets"
+                  onPress={() => setPage((prev) => prev + 1)}
+                />
+              )
+            }
+            renderItem={({ item }) => (
+              <Link href={`/pets/${item.id}`} key={item.id}>
+                <View style={styles.card}>
+                  <Text style={{ fontWeight: "bold" }}>{item.todo}</Text>
+                  <Text>ID: {item.id}</Text>
+                </View>
+              </Link>
+            )}
+            keyExtractor={(item) => item.id.toString()}
+          ></FlatList>
+        </View>
+      )}
     </SafeAreaProvider>
   );
 }
@@ -27,6 +80,10 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "white",
     marginTop: StatusBar.currentHeight || 0,
+  },
+  inner: {
+    gap: 10,
+    marginBottom: 25,
   },
   petList: {
     padding: 30,
@@ -49,5 +106,10 @@ const styles = StyleSheet.create({
   },
   red: {
     color: "red",
+  },
+
+  button: {
+    height: 25,
+    margin: 10,
   },
 });
